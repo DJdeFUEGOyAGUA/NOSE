6f41910c7a5b0056552468e7948665bff5dd1933
DJdeFUEGOyAGUA <darianzaninovic@gmail.com>
2025-05-16 21:35:32 +0000

    add file readme

---
+++ calculadora/cal-data.py
@@ -0,0 +1,5 @@
+num1 = 1
+num2 = 3
+op = "+"
+result = eval(f"{num1}{op}{num2}")
+print(result)
---
+++ calculadora/cal-input.py
@@ -0,0 +1,6 @@
+num1 = input("NUMERO 1 = ")
+op = input("OPERACION = ")
+num2 = input("NUMERO 2 = ")
+result = eval(f"{num1}{op}{num2}")
+
+print(f"EL RESULTADO ES = {result}")
